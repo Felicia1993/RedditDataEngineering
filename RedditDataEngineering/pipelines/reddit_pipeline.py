0ae3150790bd881ca576cb2baf809931a8aa3f43
FeliciaXie <359468250@qq.com>
2024-08-27 23:27:44 +0300

    Extract data from Reddit

---
+++ RedditDataEngineering/pipelines/reddit_pipeline.py
@@ -0,0 +1,17 @@
+from etls.reddit_etl import connect_reddit, extract_posts, load_data_to_csv, tranform_data
+from utils.constants import CLIENT_ID, OUTPUT_PATH, SECRET
+import pandas as pd
+
+
+
+def reddit_pipeline(file_name:str, subreddit: str, time_filter='day', limit=None):
+    # connecting to reddit instance
+    instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
+    # extraction
+    posts = extract_posts(instance, subreddit, time_filter, limit)
+    post_df = pd.DataFrame(posts)
+
+    # transformation
+    post_df = tranform_data(post_df)
+    # loading to csv
+    load_data_to_csv(post_df, f'{OUTPUT_PATH}/{file_name}.csv')
